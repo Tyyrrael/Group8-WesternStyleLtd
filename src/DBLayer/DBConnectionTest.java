cdde1d16e4cebdf66289763198d1ca1456f895ca
Tyyrrael <borislavdimitrov994@live.com>
2015-03-18 14:49:59 +0100

    The Code
    i fucked up on the first repository...

---
+++ src/DBLayer/DBConnectionTest.java
@@ -0,0 +1,19 @@
+package DBLayer;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class DBConnectionTest {
+
+	@Test
+	public void test() {
+		DBConnection dbCon = DBConnection.getInstance();
+		if (dbCon != null) {
+			System.out.println("Conecction to DB is ok");
+		} else {
+			fail("Can not connect to the DB");
+		}
+	}
+
+}
